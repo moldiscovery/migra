75033a39c1b87fa50b83ab695256b51cd72e6200
Silvano Cerza <silvanocerza@gmail.com>
2020-02-04 16:11:18 +0100

    Enhanced how opts are read

--- migra.sh
+++ migra.sh
@@ -1,35 +1,74 @@
 #!/bin/bash
 
 REGEX_GIT_URL="((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?"
+FILE=""
+OWNER=""
 URLS=()
 
 print_help() {
-    echo "Usage:"
-    echo "    migra git@bitbucket.org:<your_org>/<your_repo>.git"
-    echo "    migra https://<your_username>@bitbucket.org/<your_org>/<your_repo>.git"
-    echo "    migra -f <path_to_file>"
-    echo "    The used file must have one URL per line for the script to correctly work"
+    echo "Usage: migra -o [OWNER] [GIT_URLS]..."
+    echo "  or:  migra -o [OWNER] -f [FILE]"
+    echo "The used file must have one URL per line for the script to correctly work"
     echo
-
     echo "Options:"
-    echo "    --help, -h: Prints this text"
-    echo "    --file, -f: Path to file to parse for URLs"
-}
-
-# Check if executable is installed
-check_is_installed() {
-    eval "which $1 >/dev/null"
-    if [ $? -ne 0 ]; then
-        echo
-        echo "Please install git"
-        exit 1
-    fi
+    echo "    --help,   -h:  Prints this text"
+    echo "    --owner,  -o:  User or organization that will be used to create the new repo"
+    echo "    --file,   -f:  Path to file to parse for URLs"
+    exit 0
 }
 
 validate_git_url() {
     echo "$1" | grep -P -q $REGEX_GIT_URL
     return $?
 }
+
+# Get opts
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -o|--owner)
+            OWNER="$2"
+            shift # past argument
+            shift # past value
+        ;;
+        -f|--file)
+            FILE="$2"
+            shift # past argument
+            shift # past value
+        ;;
+        -h|--help)
+            print_help
+        ;;
+        *)
+            # Saves valid URLs
+            validate_git_url $1
+            if [ $? -eq 0 ]; then
+                URLS+=("$1")
+            else
+                echo "Ignoring invalid git URL $1"
+            fi
+            shift # past argument
+        ;;
+    esac
+done
+
+if [ -z "$OWNER" ]; then
+    echo "Owner is not optional"
+    exit 1
+fi
+
+# Check if executable is installed
+check_is_installed() {
+    eval "which $1 >/dev/null"
+    if [ $? -ne 0 ]; then
+        echo
+        echo "$1 not found, please install or add it to PATH"
+        exit 1
+    fi
+}
+
+check_is_installed git
+check_is_installed hub
 
 read_file() {
     i=1
@@ -44,30 +83,10 @@ read_file() {
     done < "$1"
 }
 
-check_is_installed git
-check_is_installed hub
-
-if [ $# -eq 0 ] \
-|| [ "$1" == "--help" ] \
-|| [ "$1" == "-h" ]
-then
-    print_help
-    exit 0
-fi
-
-if [ "$1" == "--file" ] || [ "$1" == "-f" ]; then
-    if [ -z "$2" ]; then
-        echo "Option $1 requires an argument"
+if [ -n "$FILE" ]; then
+    if [ ! -f "$FILE" ]; then
+        echo "File not found $FILE"
         exit 1
     fi
-    read_file "$2"
+    read_file "$FILE"
 fi
-
-for url in "$@"; do
-    validate_git_url $url
-    if [ $? -eq 0 ]; then
-        URLS+=("$url")
-    else
-        echo "Ignoring invalid git URL $url"
-    fi
-done
